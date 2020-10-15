17b760d39b09403094dd5301d86f2bb27f349d10
Adam-Wallenrod <psredniawa@gmail.com>
2020-10-15 22:47:55 +0200

    Created Task and ModifiableTask classes;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Task} from './task';
+import {ModifiableTask} from './modifiable-task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'to-do-app';
+  testTask = new ModifiableTask('First task', 'This is my first task ever.');
+  tasks: ModifiableTask[] = [];
+  isCreateMode: boolean;
+
+  ngOnInit() {
+    this.tasks.push(this.testTask);
+    console.log('testTask: ', this.testTask);
+  }
+
+
 }
