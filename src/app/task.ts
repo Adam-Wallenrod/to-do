9415f4e3108dee1f6019cca0ec989fe01e0dbe28
Adam-Wallenrod <psredniawa@gmail.com>
2020-10-18 19:48:15 +0200

    Added static method allowing to create unique ids;

--- src/app/task.ts
+++ src/app/task.ts
@@ -1,13 +1,26 @@
 export class Task {
 
+  static latestId: number;
+  protected id: number;
   protected title: string;
   protected content: string;
   protected timeStamp: number;
+
 
   constructor(title: string, content: string) {
     this.title = title;
     this.content = content;
     this.timeStamp = this.getTimeTicks();
+    this.id = Task.incrementId();
+  }
+
+  static incrementId() {
+    if (!this.latestId) {
+      this.latestId = 1;
+    } else {
+      ++this.latestId;
+    }
+    return this.latestId;
   }
 
   public getTitle(): string {
@@ -24,7 +37,9 @@ export class Task {
     return this.timeStamp;
   }
 
-
+  public getId(): number {
+    return this.id;
+  }
 
 
   private getTimeTicks(): number {
